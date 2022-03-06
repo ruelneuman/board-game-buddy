7f56fd7f6cdf37c349559097cfce93c8b42925ef
Ruel Neuman <ruelneuman@gmail.com>
2022-03-06 16:08:46 +0000

    remove additional express import on server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,5 @@
 import { PORT } from './config';
-import express from 'express';
 import app from './app';
-
-app.use(express.json());
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}}`);
