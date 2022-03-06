a7624bde2189e416027d37e315b0a2b5ebb185d2
Ruel Neuman <ruelneuman@gmail.com>
2022-03-06 16:06:55 +0000

    add morgan middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 
 const app = express();
 
 app.use(express.json());
+
+const morganFormat = process.env.NODE_ENV === 'production' ? 'combined' : 'dev';
+app.use(morgan(morganFormat));
 
 export default app;
