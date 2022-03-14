05a22d783bfd15a76d6345bbcf77616dcf98a5f7
Ruel Neuman <ruelneuman@gmail.com>
2022-03-14 16:23:02 +0000

    update config

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,3 +1,9 @@
 import 'dotenv/config';
 
-export const { PORT = 3001, SECRET, MONGODB_URI = '' } = process.env;
+export const {
+  PORT = 3001,
+  SECRET,
+  MONGODB_URI = '',
+  JWT_ISSUER = '',
+  JWT_AUDIENCE = '',
+} = process.env;
