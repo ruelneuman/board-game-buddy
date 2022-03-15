e61fcb1ae80f17bb43702eee28b42f6511576757
Ruel Neuman <ruelneuman@gmail.com>
2022-03-15 21:23:11 +0000

    add auth middleware

---
+++ server/src/middleware/index.ts
@@ -0,0 +1,10 @@
+import expressJwt from 'express-jwt';
+import { SECRET, JWT_AUDIENCE, JWT_ISSUER } from '../config';
+
+// eslint-disable-next-line import/prefer-default-export
+export const checkJwtAuth = expressJwt({
+  secret: SECRET,
+  audience: JWT_AUDIENCE,
+  issuer: JWT_ISSUER,
+  algorithms: ['HS256'],
+});
