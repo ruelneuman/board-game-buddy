f09bb5414c471a5303dec60de66e3cede74ad220
Ruel Neuman <ruelneuman@gmail.com>
2022-04-06 22:13:37 +0100

    remove route for creating new game

--- server/src/routes/games.router.ts
+++ server/src/routes/games.router.ts
@@ -1,11 +1,10 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
 import controller from '../controllers/games.controller';
-import { expressJwtAuth } from '../middleware/auth';
 
 const router = Router();
 
 router.get('/', controller.getGames);
-router.post('/', expressJwtAuth, controller.createGame);
 
 router.get('/:gameId', controller.getGame);
 
