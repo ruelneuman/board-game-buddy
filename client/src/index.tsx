885dfee13d2e2a8484d036e27fd360262234fa22
Ruel Neuman <ruelneuman@gmail.com>
2022-05-13 13:25:59 +0300

    configure redux store

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
 import App from './App';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -9,8 +11,10 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
