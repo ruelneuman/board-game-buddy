77723b1435d5ccd09bb79df170b85470f96b84dc
Ruel Neuman <ruelneuman@gmail.com>
2022-05-25 16:36:42 +0300

    create initial layout for games page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
+import Container from '@mui/material/Container';
+import Games from './pages/Games';
 
 function App() {
   return (
     <>
       <nav>
-        <NavLink to="/search">Search Games</NavLink>
+        <NavLink to="/games">Games</NavLink>
         <NavLink to="/users">Users</NavLink>
       </nav>
-
-      <Routes>
-        <Route path="search" element={<div>Search Games</div>} />
-        <Route path="users" element={<div>Users</div>} />
-        <Route path="/" element={<Navigate to="/search" replace />} />
-        <Route path="*" element={<div>Not Found</div>} />
-      </Routes>
+      <Container>
+        <Routes>
+          <Route path="games" element={<Games />} />
+          <Route path="users" element={<div>Users...</div>} />
+          <Route path="/" element={<Navigate to="/games" replace />} />
+          <Route path="*" element={<div>Not Found</div>} />
+        </Routes>
+      </Container>
     </>
   );
 }
---
+++ client/src/pages/Games.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import CardHeader from '@mui/material/CardHeader';
+import CardActionArea from '@mui/material/CardActionArea';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import Avatar from '@mui/material/Avatar';
+import GroupsIcon from '@mui/icons-material/Groups';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+function GameCard() {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardHeader
+          avatar={
+            <Avatar sx={{ bgcolor: 'primary.main' }} aria-label="rating">
+              9.6
+            </Avatar>
+          }
+          title="Inis"
+          subheader="2016"
+          titleTypographyProps={{ variant: 'h5', component: 'div' }}
+        />
+        <CardMedia
+          component="img"
+          image="https://d2k4q26owzy373.cloudfront.net/150x150/games/uploaded/1608452604536"
+          alt="Inis"
+          sx={{ maxHeight: 150, width: 'auto', margin: 'auto' }}
+        />
+        <CardContent>
+          <Stack direction="row" alignItems="center" gap={1}>
+            <GroupsIcon />
+            <Typography variant="body1">2-4</Typography>
+          </Stack>
+          <Stack direction="row" alignItems="center" gap={1}>
+            <AccessTimeIcon />
+            <Typography variant="body1">30-180</Typography>
+          </Stack>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+function Games() {
+  return (
+    <Grid container spacing={{ xs: 2, md: 3 }}>
+      {Array.from(Array(24)).map((_, index) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+          <GameCard />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
+
+export default Games;
